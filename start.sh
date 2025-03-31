0cfdf7cebc38f707f2ac86ef36ffa40bb5e9436a
Dimitar Milkov <dimemilkov1@gmail.com>
2025-03-31 16:36:28 +0200

    deploy changes

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Use the PORT environment variable if set by the platform, otherwise default to 8000
 PORT=${PORT:-8000}
-uvicorn app.main:app --host 0.0.0.0 --port $PORT 
-PYTHON_VERSION=3.12.0
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
